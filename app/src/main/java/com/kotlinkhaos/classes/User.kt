fda8d3c6548a9ebce5026732331bcedfef09eeda
Maximo Guk <maximo.guk@dal.ca>
2023-10-11 13:29:58 -0300

    Implement login and basic input validation

--- app/src/main/java/com/kotlinkhaos/classes/User.kt
+++ app/src/main/java/com/kotlinkhaos/classes/User.kt
@@ -1,15 +1,38 @@
 package com.kotlinkhaos.classes
 
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseUser
+import com.kotlinkhaos.classes.errors.FirebaseAuthError
 import kotlinx.coroutines.tasks.await
 
 class User private constructor(private val fireBaseUser: FirebaseUser) {
     companion object {
+        private fun validateLoginParameters(email: String, pass: String) {
+            if (email.isEmpty() || pass.isEmpty()) {
+                throw FirebaseAuthError("Email and password must not be empty")
+            }
+        }
+
         suspend fun login(email: String, pass: String): User? {
-            val mAuth = FirebaseAuth.getInstance()
-            val result = mAuth.signInWithEmailAndPassword(email, pass).await() ?: return null
-            return User(result.user!!)
+            try {
+                validateLoginParameters(email, pass)
+                val mAuth = FirebaseAuth.getInstance()
+                val result = mAuth.signInWithEmailAndPassword(email, pass).await() ?: return null
+                return User(result.user!!)
+            } catch (err: Exception) {
+                if (err is FirebaseAuthInvalidCredentialsException && err.message != null) {
+                    throw FirebaseAuthError(err.message!!)
+                }
+                if (err is FirebaseException) {
+                    if (err.message == "An internal error has occurred. [ INVALID_LOGIN_CREDENTIALS ]") {
+                        throw FirebaseAuthError("Invalid password")
+                    }
+                    throw FirebaseAuthError("An error occurred when logging in")
+                }
+                throw err
+            }
         }
 
         fun getUser(): User? {
@@ -18,6 +41,7 @@ class User private constructor(private val fireBaseUser: FirebaseUser) {
             loadedFirebaseUser.reload()
             return User(loadedFirebaseUser)
         }
+
     }
 
     suspend fun getJwt(): String {
