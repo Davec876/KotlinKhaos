385f30438bbfe012195c0e71d3662a75b1d03a9c
Maximo Guk <maximo.guk@dal.ca>
2023-10-11 13:25:06 -0300

    Refactoring and implement kotlinapi http client

---
+++ app/src/main/java/com/kotlinkhaos/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.kotlinkhaos
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kotlinkhaos.classes.PracticeQuiz
+import com.kotlinkhaos.classes.User
+import com.kotlinkhaos.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_practice, R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        CoroutineScope(Dispatchers.Main).launch {
+            val user = User.getUser()
+            if (user == null) {
+                moveToLoginPage()
+                Log.i("Firebase", "User is not logged in!")
+                return@launch
+            }
+            val practiceQuiz = PracticeQuiz.start(user, "random")
+            println(practiceQuiz.getId())
+            println(practiceQuiz.getQuestion())
+//            practiceQuiz.sendAnswer(user, "I hate kotlin")
+//            println(practiceQuiz.getFeedback())
+        }
+    }
+
+    private fun moveToLoginPage() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/kotlinkhaos/classes/services/KotlinKhaosPracticeQuizApi.kt
@@ -0,0 +1,116 @@
+package com.kotlinkhaos.classes.services;
+
+import android.util.Log
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+class KotlinKhaosPracticeQuizApi(private val token: String) {
+    private val apiHost = "https://kotlin-khaos-api.maximoguk.com"
+    private val client: HttpClient = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+            })
+        }
+    }
+
+    suspend fun startPracticeQuiz(prompt: String): PracticeQuizStartRes {
+        try {
+            val parsedRes: PracticeQuizStartRes = client.post("$apiHost/practice-quizs") {
+                parameter("prompt", prompt)
+                addRequiredApiHeaders()
+            }.body()
+            return parsedRes
+        } catch (err: Exception) {
+            Log.e("KotlinKhaosApi", "Error in startPracticeQuiz", err)
+            throw err
+        } finally {
+            client.close()
+        }
+    }
+
+    suspend fun getPracticeQuizById(
+        practiceQuizId: String
+    ): PracticeQuizGetByIdRes {
+        try {
+            val parsedRes: PracticeQuizGetByIdRes =
+                client.get("$apiHost/practice-quizs/$practiceQuizId") {
+                }.body()
+            return parsedRes
+        } catch (err: Exception) {
+            Log.e("KotlinKhaosApi", "Error in getPractice", err)
+            throw err
+        } finally {
+            client.close()
+        }
+    }
+
+    suspend fun sendPracticeQuizAnswer(
+        practiceQuizId: String,
+        answer: String
+    ): PracticeQuizAnswerRes {
+        try {
+            val parsedRes: PracticeQuizAnswerRes =
+                client.post("$apiHost/practice-quizs/$practiceQuizId") {
+                    setBody(PracticeQuizAnswerReq(answer))
+                    addRequiredApiHeaders()
+                }.body()
+            return parsedRes
+        } catch (err: Exception) {
+            Log.e("KotlinKhaosApi", "Error in sendPracticeQuizAnswer", err)
+            throw err
+        } finally {
+            client.close()
+        }
+    }
+
+    suspend fun continuePracticeQuiz(
+        practiceQuizId: String
+    ): PracticeQuizContinueRes {
+        try {
+            val parsedRes: PracticeQuizContinueRes =
+                client.post("$apiHost/practice-quizs/$practiceQuizId/continue") {
+                    addRequiredApiHeaders()
+                }.body()
+            return parsedRes
+        } catch (err: Exception) {
+            Log.e("KotlinKhaosApi", "Error in continuePracticeQuiz", err)
+            throw err
+        } finally {
+            client.close()
+        }
+    }
+
+    private fun HttpRequestBuilder.addRequiredApiHeaders() {
+        header(HttpHeaders.Authorization, "Bearer $token")
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+    }
+}
+
+@Serializable
+data class PracticeQuizStartRes(val problem: String, val practiceQuizId: String)
+
+@Serializable
+data class PracticeQuizGetByIdRes(val message: String?, val score: Int?)
+
+@Serializable
+data class PracticeQuizAnswerReq(val answer: String)
+
+@Serializable
+data class PracticeQuizAnswerRes(val feedback: String)
+
+@Serializable
+data class PracticeQuizContinueRes(val problem: String?, val score: Int?)
