5874d2a44957df88620e2a3c92b4687f8d9b0ad9
Mostafaa Abdelaziz <MostafaaAbdelaziz@dal.ca>
2023-11-16 16:00:18 -0400

    register function complete

--- app/src/main/java/com/kotlinkhaos/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/kotlinkhaos/ui/auth/RegisterFragment.kt
@@ -1,12 +1,19 @@
 package com.kotlinkhaos.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.kotlinkhaos.MainActivity
+import com.kotlinkhaos.classes.errors.FirebaseAuthError
+import com.kotlinkhaos.classes.user.User
+import com.kotlinkhaos.classes.user.UserType
 import com.kotlinkhaos.databinding.FragmentRegisterBinding
+import kotlinx.coroutines.launch
 
 class RegisterFragment : Fragment() {
     private var _binding: FragmentRegisterBinding? = null
@@ -23,9 +30,42 @@ class RegisterFragment : Fragment() {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textRegister
-        textView.text = "This is register Fragment"
+        binding.registerButton.setOnClickListener {
+            handleRegister()
+        }
         return root
+    }
+
+    private fun handleRegister(){
+        lifecycleScope.launch {
+            try {
+                val name = binding.inputName.text.toString().trim()
+                val email = binding.inputEmailAddress.text.toString().trim()
+                val password = binding.inputPassword.text.toString().trim()
+
+                val userType = if (!binding.switchChoice.isChecked) {
+                    UserType.STUDENT
+                } else {
+                    UserType.INSTRUCTOR
+                }
+
+                val user = User.register(email, password, name, userType)
+
+
+                if (user != null) {
+                    val intent = Intent(requireActivity(), MainActivity::class.java)
+                    startActivity(intent)
+                    requireActivity().finish()
+                }
+
+            } catch (err: Exception) {
+                if (err is FirebaseAuthError) {
+                    binding.errorMessage.text = err.message
+                    return@launch
+                }
+                binding.errorMessage.text = err.message
+            }
+        }
     }
 
     override fun onDestroyView() {
